9fc49a784e60ea34b5ba6fb8c1320b18a20ead7a
taofeeq billyhadiat <billyhadiattaofeeq@gmail.com>
2025-06-14 08:34:29 +0100

    Added modification yo restarant.ts

--- src/models/Restaurant.ts
+++ src/models/Restaurant.ts
@@ -1,24 +1,25 @@
-// models/City.ts
+// models/Restaurant.ts
 import mongoose, { Schema, Document, Types } from 'mongoose';
 
 export interface IRestaurant extends Document {
   name: string;
+  city: Types.ObjectId;
+  city_id?: Types.ObjectId; // optional if used
+  user_id: Types.ObjectId;
 }
 
 const RestaurantSchema: Schema = new Schema({
   name: { type: String, required: true },
-   city: {
+  city_id: {
     type: Schema.Types.ObjectId,
-    ref: 'cities', // <- This is the relationship
+    ref: 'cities', // Might be a duplicate of `city`, clarify usage
+    required: false // optional in case not always present
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'users', // References the 'users' collection
     required: true,
   },
-   user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'users', // <- This is the relationship
-    required: true,
-  },
-   address: { type: String, required: true },
 });
 
 export const RestaurantModel = mongoose.model<IRestaurant>('restaurants', RestaurantSchema);
-
